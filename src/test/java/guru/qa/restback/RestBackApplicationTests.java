92a9ca97cee67b0b446cbe38d9a92f50372b5554
andreydmitr21 <andreydmitr21@gmail.com>
2022-08-10 23:30:36 +0200

    initial

--- src/test/java/guru/qa/restback/RestBackApplicationTests.java
+++ src/test/java/guru/qa/restback/RestBackApplicationTests.java
@@ -1,13 +1,30 @@
 package guru.qa.restback;
 
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
 
 @SpringBootTest
 class RestBackApplicationTests {
 
 	@Test
-	void contextLoads() {
+	@Severity(SeverityLevel.BLOCKER)
+	@DisplayName("setSalaryById - positive test : right userId")
+	void setSalaryByIdTestPositive() {
+		given()
+				.formParam("userId","1")
+				.when()
+				.log().all()
+				.get("user/getSalary")
+
+				.then()
+				.log().all()
+				.statusCode(200)
+		;
 	}
 
 }
